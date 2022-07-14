bd84bd95353348b6a8b5a105def874ae6abb27f4
C.J. Windisch <chrisjwindisch@gmail.com>
2022-07-13 21:33:38 -0700

    calendar is clickable but can't create events yet

--- src/App.js
+++ src/App.js
@@ -1,47 +1,48 @@
-import logo from './logo.svg'
 import './App.css'
-import React from 'react'
+import React, { useState } from 'react'
 import moment from 'moment'
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 // calendar plugins
 import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
 
 
 function App() {
-  const createEvent = (start, end, jsEvent, view) => {
+  const [events, setEvents] = useState([])
+
+  const createEvent = ( selectionInfo ) => {
+    console.log("HERE")
+    const start = selectionInfo.start
+    const end = selectionInfo.end
+
     var abc = prompt('Enter Title')
     var allDay = !start.hasTime && !end.hasTime
     var newEvent = new Object()
     newEvent.title = abc
     newEvent.start = moment(start).format()
     newEvent.allDay = false
-    //$('#calendar').fullCalendar('renderEvent', newEvent)
+    console.log(newEvent)
+    setEvents(events.push(newEvent))
   }
 
-  /*
-  const start = moment().subtract(5, 'hours').format('MMMM Do YYYY, h:mm:ss a')
-  const end = moment().add(2, 'hours').format('MMMM Do YYYY, h:mm:ss a')
-  */
-  const start = moment().subtract(5, 'hours').toDate()
-  const end = moment().add(2, 'hours').toDate()
+  const handleDateClick = ( arg ) => { // bind with an arrow function
+    alert(arg.dateStr)
+  }
 
   return (
     <div className="App">
       <FullCalendar
-        plugins={[ timeGridPlugin ]}
+        plugins={[ timeGridPlugin, interactionPlugin ]}
         initialView='timeGridWeek'
         slotMinTime="07:00:00"
         slotMaxTime="20:00:00"
+        selectable={true}
+        editable={true}
+        eventResizableFromStart={true}
         select={createEvent}
+        dateClick={handleDateClick}
         timezone="PDT"
-        events={[
-          {
-            id: 'a',
-            title: 'my event',
-            start: start,
-            end: end,
-          }
-        ]}
+        events={events}
       />
     </div>
   )
