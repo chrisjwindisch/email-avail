0298ac40dfbd9f6f42e54b6557f41db92cf67141
C.J. Windisch <chrisjwindisch@gmail.com>
2022-07-13 22:09:40 -0700

    prints formatted availabilities to command line

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import './App.css'
-import React, { useState } from 'react'
-import moment from 'moment'
+import React, { useState, useEffect, useRef } from 'react'
+//import moment from 'moment'
+//import momenttz from 'moment-timezone'
+import moment from 'moment-timezone'
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 // calendar plugins
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
 
+// From: https://thewebdev.info/2021/05/24/how-to-listen-for-key-press-for-document-in-react-js/#:~:text=js-,To%20listen%20for%20keypresses%20on%20the%20whole%20document%20in%20React,document%20in%20the%20useEffect%20hook.&text=We%20create%20the%20useEventListener%20hook,eventName%20is%20the%20event%20name.
+const useEventListener = (eventName, handler, element = window) => {
+  const savedHandler = useRef();
+
+  useEffect(() => {
+    savedHandler.current = handler;
+  }, [handler]);
+
+  useEffect(() => {
+    const eventListener = (event) => savedHandler.current(event);
+    element.addEventListener(eventName, eventListener);
+    return () => {
+      element.removeEventListener(eventName, eventListener);
+    };
+  }, [eventName, element]);
+};
 
 function App() {
   const [events, setEvents] = useState([])
+  const ENTER_KEYS = ["13", "Enter"];
 
   const createEvent = ( selectionInfo ) => {
     const start = selectionInfo.start
@@ -25,9 +44,21 @@ function App() {
     setEvents(newEvents)
   }
 
-  const handleDateClick = ( arg ) => { // bind with an arrow function
-    alert(arg.dateStr)
-  }
+  const handler = ({ key }) => {
+    if (ENTER_KEYS.includes(String(key))) {
+      events.forEach((event) => {
+        const start = moment(event.start).format("ddd (M/D): H:mma")
+        const end = moment(event.end).format("H:mma zz")
+        //const timezone = moment.tz.Zone.abbr
+        const timezone = moment().tz(moment.tz.guess()).format('z');
+        const range = `${start}-${end} ${timezone}`
+        console.log(range)
+      })
+    }
+  };
+
+  useEventListener("keydown", handler);
+
 
   return (
     <div className="App">
@@ -40,7 +71,6 @@ function App() {
         editable={true}
         eventResizableFromStart={true}
         select={createEvent}
-        dateClick={handleDateClick}
         timezone="PDT"
         events={events}
       />
